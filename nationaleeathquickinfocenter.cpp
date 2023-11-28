5defaf63607e857b0e2cc88f7b51470f5b0f1bae
MOMIN ZAHOOR <98146838+CodeWithMomin@users.noreply.github.com>
2023-11-28 22:06:26 +0530

    Add files via upload
    first commit

---
+++ nationaleeathquickinfocenter.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float n; // n will hold the value of richter scale number
+    cout<<"Enter the richter scale number"<<endl;
+    cin>>n;
+    if(n<5.0)
+    cout<<"Little or no Demage"<<endl;
+    else if(n>5.0 && n<5.5)
+    cout<<"Some Demage"<<endl;
+    else if(n>5.5 &&n<6.5)
+    cout<<"Serious Demage:walls may crack or fall"<<endl;
+    else if(n>6.5 && n<7.5)
+    cout<<"Disaster:houses and Buildings may collapse"<<endl;
+    else
+    cout<<"Catastrophe: most of the Buildings destroyed"<<endl;
+    
+}
