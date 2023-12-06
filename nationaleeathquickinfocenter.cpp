62708aacf635cbd6f6493e5aa1eff64c8f99f742
Arsalan <69842668+arsalankazmi7@users.noreply.github.com>
2023-12-06 23:21:15 +0530

    Update nationaleeathquickinfocenter.cpp
    Added equal to signs at multiple places otherwise if we put input as 5, directly jumped to else condition.
    
    Also corrected the spelling from "Demage" to "Damage"

--- nationaleeathquickinfocenter.cpp
+++ nationaleeathquickinfocenter.cpp
@@ -5,13 +5,13 @@ int main()
     float n; // n will hold the value of richter scale number
     cout<<"Enter the richter scale number"<<endl;
     cin>>n;
-    if(n<5.0)
-    cout<<"Little or no Demage"<<endl;
-    else if(n>5.0 && n<5.5)
-    cout<<"Some Demage"<<endl;
-    else if(n>5.5 &&n<6.5)
-    cout<<"Serious Demage:walls may crack or fall"<<endl;
-    else if(n>6.5 && n<7.5)
+    if(n<=5.0)    //added equal to sign here otherwise not working fine
+    cout<<"Little or no Damage"<<endl;    //Corrected the "Damage" spelling
+    else if(n>5.0 && n<=5.5)    //Added equal to sign
+    cout<<"Some Damage"<<endl;
+    else if(n>5.5 && n<=6.5)    //Added equal to sign
+    cout<<"Serious Damage:walls may crack or fall"<<endl;
+    else if(n>6.5 && n<=7.5)    //Added equal to sign
     cout<<"Disaster:houses and Buildings may collapse"<<endl;
     else
     cout<<"Catastrophe: most of the Buildings destroyed"<<endl;
