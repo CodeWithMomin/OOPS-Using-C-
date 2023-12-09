b1f8a189c52a640717e92cdf52677e966955fc49
MOMIN ZAHOOR <98146838+CodeWithMomin@users.noreply.github.com>
2023-12-09 09:57:37 +0530

    final commit flag_controlled_loop.cpp

--- flag_controlled_loop.cpp
+++ flag_controlled_loop.cpp
@@ -1,18 +1,23 @@
-// write a program to print natural number from 1 to n using flag controlled loop#include<iostream>
+// write a program to print natural number from 1 to n using flag controlled loop
+#include<iostream>
 using namespace std;
 
 int main() {
-    int n;
+    int i=1, n;
     cout << "Enter a positive integer: ";
     cin >> n;
 
     bool flag = true;
 
-    for(int i = 1; flag; i++) {
-        cout << i << " ";
-
-        if(i == n)
+        while(flag)
+            {
+                cout<<i<<endl;
+                 
+        if(i>=n)     //  when loop will reach at i=100 it will terminate infinite loop by making flag false 
             flag = false;
+          else
+          i++;
+               
     }
 
     return 0;
